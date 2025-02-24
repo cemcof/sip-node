30b13d64ec2e5cc1451f8e33fce3c348fd097754
Radek Veverka <radekg.veverka@gmail.com>
2025-02-24 14:11:17 +0100

    Dev better transferer

---
+++ cemproc/processing.py
@@ -0,0 +1,21 @@
+from experiment import ExperimentModuleBase, ProcessingState, ExperimentStorageEngine, ExperimentsApi
+
+
+class CryosparcProcessingHandler(ExperimentModuleBase):
+
+    def provide_experiments(self):
+        exps = ExperimentsApi(self._api_session).get_experiments_by_states(
+            processing_state=[
+                ProcessingState.UNINITIALIZED,
+                ProcessingState.READY,
+                ProcessingState.RUNNING,
+                ProcessingState.STOP_REQUESTED,
+                ProcessingState.FINALIZING]
+        )
+
+        return filter(lambda e: e.processing.engine == "cemproc" and (
+                    e.processing.node_name == "any" or e.processing.node_name == self.module_config.lims_config.node_name),
+                      exps)
+
+    def step_experiment(self, exp_engine: ExperimentStorageEngine):
+        pass
---
+++ cemproc/tomo.py
@@ -0,0 +1,3 @@
+
+class TomoPipeline():
+    pass
